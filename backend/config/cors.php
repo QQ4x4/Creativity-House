ed6ab2d3ed8e9409285a2294c88c505e0b9ef03e
QQ4x4 <qusay.zabarah.2oo2@gmail.com>
2026-08-02 16:29:36 +0300

    Connect frontend to live Railway backend 2

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -16,6 +16,7 @@ return [
     'allowed_origins' => [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'https://creativity-house.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
